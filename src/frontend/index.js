832561e2dae8a0e568fc6d299dd51b05854db868
FrcGustavo <hidalgo_fco520@outlook.com>
2020-05-29 01:45:41 -0500

    👷‍♂️[Add] index of frontend

---
+++ src/frontend/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello World!</h1>,
+  document.getElementById('app'),
+);
