b4e4f2584a6e03804365078353f475d9de5d1910
FrcGustavo <hidalgo_fco520@outlook.com>
2020-05-29 01:54:54 -0500

    👷‍♂️[Add] config to server

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,18 @@
 import express from 'express';
+
+import loadConfigDev from './utils/loadConfigDev';
+import loadConfigProd from './utils/loadConfigProd';
 import config from './config';
 import { info } from './utils/debug';
 
 const app = express();
+
+if (config.srv.env === 'development') {
+  info('Load development config');
+  loadConfigDev(app);
+} else {
+  loadConfigProd(app);
+}
 
 app.listen(config.srv.port, () => {
   info(`srv is listening on http://localhost:${config.srv.port}`);
