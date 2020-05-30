ca160b4c21990fc66b164f494e348c97677844bb
FrcGustavo <hidalgo_fco520@outlook.com>
2020-05-29 19:54:46 -0500

    👷‍♂️[Add] redux in frontend

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,9 +1,25 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router } from 'react-router';
+import { Provider } from 'react-redux';
+import { createStore, compose } from 'redux';
+import { createBrowserHistory } from 'history';
+
+import reducer from './reducers/index';
 import App from './routes/App';
 
+const history = createBrowserHistory();
+const preloadedState = window.__PRELOADED_STATE__ || {};
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, preloadedState, composeEnhancers());
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('app'),
 );
