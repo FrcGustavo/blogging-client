d6d7a3853f5827bf3f0add275e883446b59ac76a
FrcGustavo <hidalgo_fco520@outlook.com>
2020-05-29 19:59:16 -0500

    👷‍♂️[Add] redux in backend

--- src/server/utils/renderApp.js
+++ src/server/utils/renderApp.js
@@ -2,23 +2,28 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config';
+import { createStore } from 'redux';
 
 import Layout from '../../frontend/components/Layout';
-
+import reducer from '../../frontend/reducers/index';
 import serverRoutes from '../../frontend/routes/serverRoutes';
 
 import setResponse from './setResponse';
 
 const renderApp = (initialState, url, manifest, metaTags) => {
+  const store = createStore(reducer, initialState);
   const html = renderToString(
-    <StaticRouter location={url} context={{}}>
-      <Layout>
-        { renderRoutes(serverRoutes) }
-      </Layout>
-    </StaticRouter>,
+    <Provider store={store}>
+      <StaticRouter location={url} context={{}}>
+        <Layout>
+          { renderRoutes(serverRoutes) }
+        </Layout>
+      </StaticRouter>
+    </Provider>,
   );
-  const preloadedState = {};
+  const preloadedState = store.getState();
   return setResponse(html, preloadedState, manifest, metaTags);
 };
 
