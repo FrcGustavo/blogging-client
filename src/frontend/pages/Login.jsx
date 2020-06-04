4118fa588032f20b06544a250227b779339ebb3a
FrcGustavo <hidalgo_fco520@outlook.com>
2020-06-04 01:59:12 -0500

    👷‍♂️[Add] page Login

--- src/frontend/pages/Login.jsx
+++ src/frontend/pages/Login.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
-const Login = () => (<p>Login</p>);
+const Login = () => (
+  <main className="login">
+    <div className="login-form">
+      <h1>Inicia Session</h1>
+      <form action="">
+        <div className="input-form">
+          <input type="text" placeholder="Correo" />
+        </div>
+        <div className="input-form">
+          <input type="text" placeholder="Contraseña" />
+        </div>
+        <div>
+          <input type="submit" className="btn btn-primary" value="entar" />
+        </div>
+      </form>
+    </div>
+  </main>
+);
 
 export default Login;
