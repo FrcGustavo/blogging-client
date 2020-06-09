69aa85fa2176b57b5e6df284ec0b58b2bcfeaf6a
FrcGustavo <hidalgo_fco520@outlook.com>
2020-06-09 16:59:07 -0500

    👷‍♂️[Add] Model of User

---
+++ src/frontend/models/UserModel.js
@@ -0,0 +1,28 @@
+class UserModel {
+  constructor({
+    _id,
+    firstName,
+    lastName,
+    username,
+    cover,
+    email,
+    password,
+    type,
+    isDisabled,
+    createdAt,
+    updatedAt,
+  }) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.username = username;
+    this.cover = cover;
+    this.email = email;
+    this.password = password;
+    this.type = type;
+    this.isDisabled = isDisabled;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+
+}
