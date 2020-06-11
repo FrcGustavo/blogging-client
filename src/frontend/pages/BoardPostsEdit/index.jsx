636ed3bac2998a8ca9ed6479303f2f64e9d11c7b
FrcGustavo <hidalgo_fco520@outlook.com>
2020-06-11 14:33:34 -0500

    👷‍♂️[Add] Form to edit post

--- src/frontend/pages/BoardPostsEdit/index.jsx
+++ src/frontend/pages/BoardPostsEdit/index.jsx
@@ -1,7 +1,41 @@
 import React from 'react';
 
-const BoardPostsEdit = () => (
-  <h1>Editar</h1>
-);
+import PostModel from '../../models/PostModel';
+
+import './styles.scss';
+
+const BoardPostsEdit = () => {
+  const post = new PostModel({});
+  return (
+    <div className="board-posts">
+      <div className="board-posts-form">
+        <form action="">
+          <div className="input-form">
+            <input type="text" placeholder="Titulo" value={post.title} />
+          </div>
+          <div className="input-form">
+            <input type="text" placeholder="Slug" />
+          </div>
+          <div className="input-form">
+            <input type="text" placeholder="Description" />
+          </div>
+          <div className="input-form">
+            <input type="text" placeholder="KeyWords" />
+          </div>
+          <div className="editor">
+            <div className="input">
+              <textarea name="body" id="" cols="30" rows="10">
+                {post.body}
+              </textarea>
+            </div>
+            <div className="output">
+              {post.body}
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default BoardPostsEdit;
