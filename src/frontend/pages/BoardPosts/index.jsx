b5031e38df9396336213797e10edb2494f2ab7c0
FrcGustavo <hidalgo_fco520@outlook.com>
2020-06-11 02:47:31 -0500

    👷‍♂️[Add] Header in BoardPosts

--- src/frontend/pages/BoardPosts/index.jsx
+++ src/frontend/pages/BoardPosts/index.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const BoardPosts = () => (
-  <h1>Publicaciones</h1>
-);
+import Button from '../../components/atoms/Button';
+
+import config from '../../config';
+
+import './styles.scss';
+
+const BoardPosts = () => {
+  const [posts, setPosts] = useState(false);
+
+  useEffect(() => {
+    if (!posts) {
+      axios.get(`${config.domain}/api/posts`)
+        .then((res) => setPosts(res.data.posts));
+    }
+  });
+  console.log(posts);
+
+  return (
+    <div className="board-posts">
+      <header className="board-posts-header">
+        <Button to="/board/posts/new">Nuevo Post</Button>
+      </header>
+    </div>
+  );
+};
 
 export default BoardPosts;
