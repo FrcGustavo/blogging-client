668f24a20e3d77215169b18f525da1bb86ae8640
FrcGustavo <hidalgo_fco520@outlook.com>
2020-06-11 15:08:56 -0500

    👷‍♂️[Add] load of post to will edit

--- src/frontend/pages/BoardPostsEdit/index.jsx
+++ src/frontend/pages/BoardPostsEdit/index.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import showdown from 'showdown';
+import axios from 'axios';
 
 import PostModel from '../../models/PostModel';
 
+import config from '../../config';
+
 import './styles.scss';
 
-const BoardPostsEdit = () => {
-  const post = new PostModel({});
+const converter = new showdown.Converter();
+
+const BoardPostsEdit = ({ match }) => {
+  console.log(match);
+
+  const [post, setPost] = useState(false);
+  const [preview, setPreview] = useState();
+  useEffect(() => {
+    if (!post) {
+      axios.get(`${config.api}/posts/${match.params.slug}`)
+        .then((res) => setPost(new PostModel(res.data.body)));
+    }
+  });
+
+  const handleChange = (e) => {
+    post.body = e.target.value;
+    setPreview(converter.makeHtml(post.body));
+  };
+
+  console.log('POST', post);
+  if (!post) return <div>Cargando...</div>;
   return (
     <div className="board-posts">
       <div className="board-posts-form">
@@ -14,28 +38,30 @@ const BoardPostsEdit = () => {
             <input type="text" placeholder="Titulo" value={post.title} />
           </div>
           <div className="input-form">
-            <input type="text" placeholder="Slug" />
+            <input type="text" placeholder="Slug" value={post.slug} />
           </div>
           <div className="input-form">
-            <input type="text" placeholder="Description" />
+            <input type="text" placeholder="Description" value={post.description} />
           </div>
           <div className="input-form">
-            <input type="text" placeholder="KeyWords" />
+            <input type="text" placeholder="KeyWords" value={post.keywords} />
           </div>
           <div className="editor">
             <div className="input">
-              <textarea name="body" id="" cols="30" rows="10">
+              <textarea name="body" onChange={handleChange}>
                 {post.body}
               </textarea>
             </div>
-            <div className="output">
-              {post.body}
-            </div>
+            <div className="output post-content" dangerouslySetInnerHTML={{ __html: preview }} />
           </div>
         </form>
       </div>
     </div>
   );
+};
+
+BoardPostsEdit.propTypes = {
+  match: PropTypes.objectOf().isRequired,
 };
 
 export default BoardPostsEdit;
