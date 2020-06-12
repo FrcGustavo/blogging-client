e316d67a05dee9adc9482705d77556971cf4a877
FrcGustavo <hidalgo_fco520@outlook.com>
2020-06-12 00:28:02 -0500

    👷‍♂️[Add] Logout

--- src/frontend/components/organisms/HeaderBoard/index.jsx
+++ src/frontend/components/organisms/HeaderBoard/index.jsx
@@ -4,17 +4,26 @@ import IconMenu from '../../atoms/IconMenu';
 
 import './styles.scss';
 
-const HeaderBoard = () => (
-  <header className="header-board">
-    <div className="header-board-container">
-      <button type="button">
-        <IconMenu />
-      </button>
-      <div>
-        Salir
-      </div>
-    </div>
-  </header>
-);
+const HeaderBoard = () => {
+  const handleLogout = () => {
+    document.cookie = 'username=';
+    document.cookie = 'email=';
+    document.cookie = 'id=';
+    document.cookie = 'token=';
+    window.location.href = '/';
+  };
 
+  return (
+    <header className="header-board">
+      <div className="header-board-container">
+        <button type="button">
+          <IconMenu />
+        </button>
+        <div>
+          <button type="button" onClick={handleLogout}>Salir</button>
+        </div>
+      </div>
+    </header>
+  );
+};
 export default HeaderBoard;
