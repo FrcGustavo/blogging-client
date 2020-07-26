6adc7bc92d2287d137819b4550171fb9e900c497
Francisco Gustavo <hidalgo_fco520@outlook.com>
2020-07-25 23:55:33 -0500

    👨‍🔧[Fix] styles

--- src/server/utils/setResponse.js
+++ src/server/utils/setResponse.js
@@ -15,8 +15,7 @@ const setResponse = (html, state, manifest, metaTags) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" type="text/css" href="${mainStyles}"/>
-        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" />
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
         <link rel="icon" type="image/ico" href="/favicon.ico">
         <title>${metaTags.title || 'FrcGustavo | Backend Software Developer '}</title>
     </head>
