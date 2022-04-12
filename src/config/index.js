6cded9803fd868af8a4b1a33f42d2de05841412d
FranciscoGustavo <hidalgofco520@gmail.com>
2022-04-12 12:45:33 -0500

    fix: change enviroment variables

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
-export const api = process.env.NEXT_PUBLIC_API;
-export const apiKey = process.env.NEXT_PUBLIC_API_KEY
-export const githubProfile = process.env.GITHUB_PROFILE
-export const linkedinProfile = process.env.LINKEDIN_PROFILE
-export const contactEmail = process.env.CONTACT_EMAIL
+export const api = process.env.API;
+export const apiKey = process.env.API_KEY
+export const githubProfile = process.env.NEXT_PUBLIC_GITHUB_PROFILE
+export const linkedinProfile = process.env.NEXT_PUBLIC_LINKEDIN_PROFILE
+export const contactEmail = process.env.NEXT_PUBLIC_CONTACT_EMAIL
