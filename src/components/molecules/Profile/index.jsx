d55ea5cbb013dcc60b1fa260870855db43c5ce98
Francisco Gustavo <gustavo@frcgustavo.com>
2023-01-08 14:44:42 -0600

    refactor: migrate Profile component to tailwind
    Signed-off-by: Francisco Gustavo <gustavo@frcgustavo.com>

--- src/components/molecules/Profile/index.jsx
+++ src/components/molecules/Profile/index.jsx
@@ -1,35 +1,34 @@
 import Image from 'next/image';
 import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 import { githubProfile, linkedinProfile, contactEmail } from 'root/config';
-import { optimizeImage } from 'root/utils';
-import { CSSProfile, CSSSocialMedia } from './styles';
+import styles from './Profile.module.css'
 
 const Profile = ({ cover, name, title, description }) => {
   return (
-    <CSSProfile>
-      <div>
-        <Image src={cover} alt={name} width={150} height={150} />
+    <section className={styles.profile}>
+      <div className='flex justify-center pb-7'>
+        <Image src={cover} alt={name} width={150} height={150} className='w-36 h-36 rounded-full' />
       </div>
       <div>
-        <h1>{name}</h1>
-        <h2>{title}</h2>
-        <p>{description}</p>
-        <CSSSocialMedia>
-          <a href={linkedinProfile} rel="noopener noreferrer" target="_blank">
-            <FaLinkedin />
+        <h1 className='mb-3 text-3xl text-white font-bold text-center sm:text-left'>{name}</h1>
+        <h2 className='mb-3 text-2xl text-white font-bold text-center sm:text-left'>{title}</h2>
+        <p className='mb-3 text-white text-center sm:text-left'>{description}</p>
+        <div className={styles.socialMedia}>
+          <a href={linkedinProfile} rel="noopener noreferrer" target="_blank" className='text-[0px]'>
+            <FaLinkedin  className='text-3xl text-primary'/>
             LinkedIn
           </a>
-          <a href={githubProfile} rel="noopener noreferrer" target="_blank">
-            <FaGithub />
+          <a href={githubProfile} rel="noopener noreferrer" target="_blank" className='text-[0px]'>
+            <FaGithub  className='text-3xl text-primary'/>
             Github
           </a>
-          <a href={`mailto:${contactEmail}?subject=frcgustavo.com`}>
-            <FaEnvelope />
+          <a href={`mailto:${contactEmail}?subject=frcgustavo.com`} className='text-[0px]'>
+            <FaEnvelope  className='text-3xl text-primary'/>
             Correo electronico
           </a>
-        </CSSSocialMedia>
+        </div>
       </div>
-    </CSSProfile>
+    </section>
   );
 };
 
