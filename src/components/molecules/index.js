f52cae7d7f3322c8ce7875245e70135743c36d09
Francisco Gustavo <gustavo@frcgustavo.com>
2023-01-08 18:35:51 -0600

    refactor: delete components don't used
    Signed-off-by: Francisco Gustavo <gustavo@frcgustavo.com>

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -3,5 +3,4 @@ export { default as Profile } from './Profile';
 export { default as HomePost } from './HomePost';
 export { default as CardPost } from './CardPost';
 export { default as Footer } from './Footer';
-export { default as Alert } from './Alert';
 export { default as Modal } from './Modal';
